ade3a4f0bec55124f0724d5a0ea865e0e0520ee3
SaneyBinFaruk <saneybinfaruk@SaneyBinFaruks-iMac.local>
2024-07-02 02:19:07 +0600

    Upload full project at once

--- src/component/PlatformIconList.tsx
+++ src/component/PlatformIconList.tsx
@@ -10,9 +10,9 @@ import {
 import { SiNintendo } from "react-icons/si";
 import { MdPhoneIphone } from "react-icons/md";
 import { BsGlobe } from "react-icons/bs";
-import { Platform } from "../hooks/useGames";
 import { HStack, Icon, Text } from "@chakra-ui/react";
 import { IconType } from "react-icons";
+import Platform from "../entities/Platform";
 
 interface Props {
   platform: Platform[];
